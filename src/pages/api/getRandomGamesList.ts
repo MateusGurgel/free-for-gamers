8e8dd4c42fa018382682893cd6540b45ba316cf1
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-04-22 14:18:20 -0300

    game type created

--- src/pages/api/getRandomGamesList.ts
+++ src/pages/api/getRandomGamesList.ts
@@ -2,14 +2,14 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import matrixToArray from '@/services/matrixToArray';
 import fetcher from '@/fecher';
 import randomIntFromInterval from '@/services/randomIntFromInterval';
-import game from '@/services/game';
+import Game from '@/services/game';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const gamesMatrix = await fetcher("https://www.freetogame.com/api/games");
     const gamesArray = matrixToArray(gamesMatrix);
 
-    let randomGames: game[] = [];
+    let randomGames: Game[] = [];
 
     for (let index = 0; index < 9; index++) {
         let randomNumber = randomIntFromInterval(0, gamesArray.length - 1);
