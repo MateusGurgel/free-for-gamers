dc344a81db4a30d58121844d061b5598527d8f4a
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-04-23 16:16:20 -0300

    whileHover animation was added

--- src/components/categoryBadge/index.tsx
+++ src/components/categoryBadge/index.tsx
@@ -1,26 +1,30 @@
 import { Flex } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 interface CategoryCardProps {
   category: string;
+  selected: boolean;
 }
 
-export function CategoryBadge({ category }: CategoryCardProps) {
+export function CategoryBadge({ category, selected }: CategoryCardProps) {
   return (
-    <Flex
-      align="center"
-      justify="center"
-      
-      bg="ui"
-      
-      paddingX="3"
-      paddingY="1"
-
-      borderRadius={8}
-
-      fontSize="lg"
-      fontWeight="light"
+    <motion.button
+    whileHover={{ scale: 1.3, borderRadius: 8, rotateZ: 3 , boxShadow: "0px 0px 2px 1px #00FFFF" }}
+    whileTap={{ scale: 1 }}
     >
-      <p>{category}</p>
-    </Flex>
+      <Flex
+        boxShadow={selected ? "0px 0px 2px 1px #00FFFF" : ""}
+        align="center"
+        justify="center"
+        bg="ui"
+        paddingX="3"
+        paddingY="1"
+        borderRadius={8}
+        fontSize="lg"
+        fontWeight="light"
+      >
+        <p>{category}</p>
+      </Flex>
+    </motion.button>
   );
 }
