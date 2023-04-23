0ea3d362dc93ccb5795aa461c3e7fbbd6d9f965c
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-04-23 13:06:51 -0300

    Infinite scrolling added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,31 @@
-import randomIntFromInterval from "@/services/randomIntFromInterval";
-import { Flex, SimpleGrid, Skeleton, Stack } from "@chakra-ui/react";
+import { SkeletonGameCard } from "@/components/SkeletonGameCard";
+import { Flex, SimpleGrid, Stack } from "@chakra-ui/react";
 import { CategoryBadge } from "@/components/categoryBadge";
 import { gameGenres } from "@/services/gameGenres";
 import { GameCard } from "@/components/GameCard";
 import { useEffect, useState } from "react";
-import fetcher from "@/fecher";
 import Game from "@/services/game";
+import fetcher from "@/fecher";
 import useSWR from "swr";
-import { SkeletonGameCard } from "@/components/SkeletonGameCard";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Home() {
   const [gameList, setGameList] = useState<Game[]>([]);
 
   const { data, isLoading } = useSWR("/api/getRandomGamesList", fetcher, {
     revalidateOnFocus: false,
-    revalidateOnMount: true,
     refreshWhenHidden: false,
     revalidateOnReconnect: false,
     refreshWhenOffline: false,
   });
 
+  async function loadMoreGames() {
+    const newItems = await fetcher("/api/getRandomGamesList");
+    setGameList((prevItems) => [...prevItems, ...newItems]);
+  }
+
   useEffect(() => {
     setGameList(data);
-    console.log("Mudou!");
   }, [data]);
 
   return (
@@ -34,34 +37,32 @@ export default function Home() {
       </Flex>
 
       <Flex justify="center">
-        <SimpleGrid columns={3} spacing={5}>
-          {!isLoading ? (
-            gameList &&
-            gameList.map((game) => (
-              <GameCard
-                key={game.id}
-                category={game.genre}
-                name={game.title}
-                description={game.short_description}
-                imageUrl={game.thumbnail}
-              />
-            ))
-          ) : (
-            <>
-              <SkeletonGameCard />
-              <SkeletonGameCard />
-              <SkeletonGameCard />
-
-              <SkeletonGameCard />
-              <SkeletonGameCard />
-              <SkeletonGameCard />
-
-              <SkeletonGameCard />
-              <SkeletonGameCard />
-              <SkeletonGameCard />
-            </>
-          )}
-        </SimpleGrid>
+        {!isLoading && gameList ? (
+          <InfiniteScroll
+            dataLength={gameList.length | 0}
+            next={loadMoreGames}
+            hasMore={true}
+            loader={<div>Loading...</div>}
+          >
+            <SimpleGrid columns={3} spacing={5}>
+              {gameList.map((game) => (
+                <GameCard
+                  key={game.id}
+                  category={game.genre}
+                  name={game.title}
+                  description={game.short_description}
+                  imageUrl={game.thumbnail}
+                />
+              ))}
+            </SimpleGrid>
+          </InfiniteScroll>
+        ) : (
+          <SimpleGrid columns={3} spacing={5}>
+            {[...Array(9)].map((_, i) => (
+              <SkeletonGameCard key={i} />
+            ))}
+          </SimpleGrid>
+        )}
       </Flex>
     </Stack>
   );
